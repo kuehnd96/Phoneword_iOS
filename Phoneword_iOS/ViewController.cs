7685025afda832ade23aac7a0e042ae18bc63c35
David Kuehn <kuehnd96@gmail.com>
2016-01-12 21:33:14 -0600

    Did a bit more to my first Tamarin iOS app.

--- Phoneword_iOS/ViewController.cs
+++ Phoneword_iOS/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Foundation;
 using UIKit;
 
 namespace Phoneword_iOS
@@ -14,7 +14,40 @@ namespace Phoneword_iOS
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            string translatedNumber = "";
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+                // Convert the phone number with text to a number
+                // using PhoneTranslator.cs
+                translatedNumber = PhoneTranslator.ToNumber(
+                    PhoneNumberText.Text);
+
+                // Dismiss the keyboard if text field was tapped
+                PhoneNumberText.ResignFirstResponder ();
+
+                if (translatedNumber == "") {
+                    CallButton.SetTitle ("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                } else {
+                    CallButton.SetTitle ("Call " + translatedNumber,
+                        UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) => {
+                // Use URL handler with tel: prefix to invoke Apple's Phone app...
+                var url = new NSUrl ("tel:" + translatedNumber);
+
+
+                // ...otherwise show an alert dialog
+                if (!UIApplication.SharedApplication.OpenUrl (url)) {
+                    var alert = UIAlertController.Create ("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                    alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController (alert, true, null);
+                }
+            };
         }
 
         public override void DidReceiveMemoryWarning()
